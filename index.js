57acecbe17b34ca58b1f0e0a27a3e25d5366e12e
pixtor <pixtor@bk.ru>
2018-09-06 15:48:06 +0500

    First commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// Для асинхронной работы используется пакет micro.
+const { json } = require('micro');
+
+// Запуск асинхронного сервиса.
+module.exports = async req => {
+
+    // Из запроса извлекаются свойства request, session и version.
+    const { request, session, version } = await json(req);
+    var returnText = 'Приветики';
+    // В тело ответа вставляются свойства version и session из запроса.
+    // Подробнее о формате запроса и ответа — в разделе Протокол работы навыка
+    if(~request.original_utterance.indexOf('андре')){
+        returnText = 'Андрей лучший проджект в мире, даже лучше Илона Маска. Алиса точно в этом уверена';
+    }
+    if(~request.original_utterance.indexOf('тимур')){
+        returnText = 'Просто лучший';
+    }
+
+    var timer = true;
+    var tree = true;
+
+    return {
+        version,
+        session,
+        response: {
+
+            // В свойстве response.text возвращается исходная реплика пользователя.
+            // Если навык был активирован без дополнительной команды,
+            // пользователю нужно сказать "Hello!".
+            text: returnText,
+
+            // Свойство response.end_session возвращается со значением false,
+            // чтобы диалог не завершался.
+            end_session: false,
+        },
+    };
+};
